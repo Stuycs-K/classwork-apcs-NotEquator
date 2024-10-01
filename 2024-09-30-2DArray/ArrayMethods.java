50654b0460e2dad39881a9a1bcd4a5eec335bf1e
Mottaqi Abedin <mottaqiabedin@gmail.com>
2024-10-01 14:47:52 -0400

    started replacenegative function

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -96,4 +96,27 @@ public class ArrayMethods {
       }
       return result;
     }
+    //3. Modify a given 2D array of integer as follows:
+    //Replace all the negative values:
+    //-When the row number is the same as the column number replace
+    //that negative with the value 1
+    //-All other negatives replace with 0
+    public static void replaceNegative(int[][] vals){
+      for (int row = 0; row < vals.length; row++) {
+        for (int column = 0; column < vals[row].length; column++) {
+          if (vals[row][column] < 0) {
+            return 0;
+          }
+        }
+      }
+    }
+
+    //4. Make a copy of the given 2d array.
+    //When testing : make sure that changing the original does NOT change the copy.
+    //DO NOT use any built in methods that "copy" an array.
+    //You SHOULD write a helper method for this.
+    //If you don't see a good way to do that, you should stop and look at prior methods.
+    public static int[][] copy(int[][] nums){
+      return null;//placeholder so it compiles
+    }
 }
