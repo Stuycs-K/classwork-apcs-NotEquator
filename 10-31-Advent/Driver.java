61016c19aab716ef490df7ddd7f71894ab2dd93b
Mottaqi Abedin <mottaqiabedin@gmail.com>
2024-11-18 19:31:41 -0500

    tested advent to work

--- 10-31-Advent/Driver.java
+++ 10-31-Advent/Driver.java
@@ -5,20 +5,36 @@ import java.util.Scanner;
 public class Driver {
     public static void main(String[] args) {
         try {
-            String fileForA = "C:\\Users\\motta\\OneDrive\\Desktop\\APCS\\classwork-apcs-NotEquator\\10-31-Advent\\inputA.txt";
-            int triangleCountA = TriangleTester.countTrianglesA(fileForA);
+            String file = "C:\\Users\\motta\\OneDrive\\Desktop\\APCS\\classwork-apcs-NotEquator\\10-31-Advent\\inputA.txt";
+            int triangleCountA = TriangleTester.countTrianglesA(file);
             System.out.println("Count of triangles (method A): " + triangleCountA);
         } catch (Exception ex) {
             System.out.println("File not found");
             return;
         }
         try {
-            String fileForB = "C:\\Users\\motta\\OneDrive\\Desktop\\APCS\\classwork-apcs-NotEquator\\10-31-Advent\\inputTri.txt";
-            int triangleCountB = TriangleTester.countTrianglesB(fileForB);
+            String file = "C:\\Users\\motta\\OneDrive\\Desktop\\APCS\\classwork-apcs-NotEquator\\10-31-Advent\\inputTri.txt";
+            int triangleCountB = TriangleTester.countTrianglesB(file);
             System.out.println("Count of triangles (method B): " + triangleCountB);
         } catch (Exception ex) {
           System.out.println("File not found");
           return;
         }
+        try {
+          String file = "C:\\Users\\motta\\OneDrive\\Desktop\\APCS\\classwork-apcs-NotEquator\\10-31-Advent\\Advent2016input.txt";
+          int triangleCountA = TriangleTester.countTrianglesA(file);
+          System.out.println("Count of triangles (method A): " + triangleCountA);
+      } catch (Exception ex) {
+          System.out.println("File not found");
+          return;
+      }
+      try {
+          String file = "C:\\Users\\motta\\OneDrive\\Desktop\\APCS\\classwork-apcs-NotEquator\\10-31-Advent\\Advent2016input.txt";
+          int triangleCountB = TriangleTester.countTrianglesB(file);
+          System.out.println("Count of triangles (method B): " + triangleCountB);
+      } catch (Exception ex) {
+        System.out.println("File not found");
+        return;
+      }
     }
 }
