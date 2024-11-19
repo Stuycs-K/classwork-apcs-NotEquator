1613f13e3f6965e7a152612199cd8adf114c0ae4
Mottaqi Abedin <mottaqiabedin@gmail.com>
2024-11-18 19:42:18 -0500

    created file

---
+++ 2024-11-18-advent2016/Advent2016Day1Part1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.io.File;
+public class Advent2016Day1Part1 {
+    public static void main(String[] args) {
+        return;
+    }
+
+}
