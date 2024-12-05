450df449dd80246ee9043868851103e63443cf5a
Mottaqi Abedin <mottaqiabedin@gmail.com>
2024-12-04 21:17:38 -0500

    created the border

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,5 +1,31 @@
+import java.util.Random;
 public class Driver {
-  public static void main(String[] args) {
-    Text.color(red)
-  }
+    public static void main(String[] args) {
+        System.out.print(Text.CLEAR_SCREEN);
+        System.out.print(Text.HIDE_CURSOR);
+        drawBorder();
+        Text.go(31, 1);
+        System.out.print(Text.RESET);
+    }
+
+    private static void drawBorder() {
+        for (int col = 1; col <= 80; col++) {
+            Text.go(1, col);
+            Text.color(Text.RED, Text.background(Text.RED));
+            System.out.print(" ");
+
+            Text.go(30, col);
+            Text.color(Text.RED, Text.background(Text.RED));
+            System.out.print(" ");
+        }
+        for (int row = 1; row <= 30; row++) {
+            Text.go(row, 1);
+            Text.color(Text.RED, Text.background(Text.RED));
+            System.out.print(" ");
+
+            Text.go(row, 80);
+            Text.color(Text.RED, Text.background(Text.RED));
+            System.out.print(" ");
+        }
+    }
 }
