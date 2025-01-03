fee711b80d54d2a64c6a7e0e523f99743ce85eb5
Mottaqi Abedin <mottaqiabedin@gmail.com>
2025-01-03 10:47:18 -0600

    started makewords

---
+++ 2025-01-02/makewordstest.java
@@ -0,0 +1,14 @@
+public class makewordstest {
+  public static void makeWords(int remainingletters, String result, String alphabet) {
+    /* if (remainingletters > 0) {
+        System.out.println(result);
+    }
+    else {
+      makeWords(remainingletters-1,result+letter,alphabet);
+    } */
+  }
+  public static void main(String[] args) {
+    // makeWords(2, "", "abc");
+
+  }
+}
